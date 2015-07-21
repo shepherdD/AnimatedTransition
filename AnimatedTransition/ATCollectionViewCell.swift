11383d9d597136d492e09f939ba0cde10fbe9de0
wudi <mailswd@sina.com>
2015-07-21 18:20:05 +0800

    xiugai

---
+++ AnimatedTransition/ATCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ATCollectionViewCell.swift
+//  AnimatedTransition
+//
+//  Created by Eddie on 15/7/21.
+//  Copyright (c) 2015年 Eddie. All rights reserved.
+//
+
+import UIKit
+
+class ATCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var textLabel: UILabel!
+    
+}
