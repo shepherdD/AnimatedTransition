f6476e6a61de169be96039910f6baa161d494395
wudi <mailswd@sina.com>
2015-07-21 13:58:43 +0800

    anim

---
+++ AnimatedTransitionTests/AnimatedTransitionTests.swift
@@ -0,0 +1,36 @@
+//
+//  AnimatedTransitionTests.swift
+//  AnimatedTransitionTests
+//
+//  Created by Eddie on 15/7/21.
+//  Copyright (c) 2015年 Eddie. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AnimatedTransitionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
